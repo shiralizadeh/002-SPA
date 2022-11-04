369e6f30ea3c5e82807e2867fd3f2a459862240d
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2022-11-04 12:29:14 +0100

    Token

--- src/components/AuthWrapper.js
+++ src/components/AuthWrapper.js
@@ -18,7 +18,7 @@ const AuthWrapper = () => {
 
   // Poor man’s auth handling
   useEffect(() => {
-    const storedToken = localStorage.getItem("github_token")
+    const storedToken = localStorage.getItem("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")
 
     if (storedToken) {
       setGithubToken(storedToken)
